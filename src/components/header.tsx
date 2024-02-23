6997e98f431dd77d91e96d45f4b6e55cc7fc09a5
Akira Otaka <hydrangeas.project@gmail.com>
2024-02-23 01:22:36 +0000

    Header作成

---
+++ src/components/header.tsx
@@ -0,0 +1,98 @@
+"use client";
+
+import Image from "next/image";
+import { Navbar } from "flowbite-react";
+import Link from "next/link";
+import { useState } from "react";
+
+export default function Header() {
+  const [openNavbar, setOpenNavbar] = useState(false);
+  const toggleNavbar = () => {
+    setOpenNavbar(!openNavbar);
+  };
+
+  return (
+    <>
+      <header>
+        <div className="md:container xl:max-w-5xl mx-auto flex flex-col">
+          <div className="flex justify-between w-full">
+            <div className="brand-logo px-4 pt-4 md:p-4">
+              <a
+                className="pointer-events-none place-items-center"
+                href="/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Image
+                  src="/DGSHAPE_Logo.png"
+                  alt="DGSHAPE Logo"
+                  className="dark:invert"
+                  width={200}
+                  height={42}
+                  priority
+                />
+              </a>
+            </div>
+            <div className="flex items-center px-4 pt-4 md:p-4">
+              <div className="mr-4">
+                <a href="#">EN</a>
+              </div>
+              <div className="mr-4 md:mr-0">
+                <a href="#">JP</a>
+              </div>
+
+              <button
+                data-collapse-toggle="navbar-default"
+                type="button"
+                className="inline-flex items-center px-2 w-10 h-10 justify-center text-sm text-gray-100 bg-gray-500 md:hidden"
+                aria-controls="navbar-default"
+                aria-expanded="false"
+                onClick={toggleNavbar}
+              >
+                <span className="sr-only">Open main menu</span>
+                <svg
+                  className="w-5 h-5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 17 14"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M1 1h15M1 7h15M1 13h15"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+
+          <nav
+            className={`${openNavbar ? "hidden" : undefined} md:block`}
+            id="navbar-default"
+          >
+            <ul className="flex flex-col text-lg px-4 md:flex-row md:justify-between md:text-base">
+              <li className="p-8 bg-gray-500 text-gray-100 md:p-0 md:bg-transparent md:text-gray-500 ">
+                <a href="#">オンラインショップ</a>
+              </li>
+              <li className="p-8 bg-gray-500 text-gray-100 md:p-0 md:bg-transparent md:text-gray-500">
+                <a href="#">製品</a>
+              </li>
+              <li className="p-8 bg-gray-500 text-gray-100 md:p-0 md:bg-transparent md:text-gray-500">
+                <a href="#">サポート</a>
+              </li>
+              <li className="p-8 bg-gray-500 text-gray-100 md:p-0 md:bg-transparent md:text-gray-500">
+                <a href="#">ニュース</a>
+              </li>
+              <li className="p-8 bg-gray-500 text-gray-100 md:p-0 md:bg-transparent md:text-gray-500">
+                <a href="#">企業情報</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+    </>
+  );
+}
